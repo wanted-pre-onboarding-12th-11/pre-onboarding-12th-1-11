127f05adde7249ec5f93b43ea6b7c0c220b21182
saem <saemilee01@gmail.com>
2023-08-24 02:19:19 +0900

    feat: 투두리스트 패칭 및 전역 상태 업데이트 로직 추가, create 컴포넌트 추가

--- src/containers/TodoList.tsx
+++ src/containers/TodoList.tsx
@@ -1,21 +1,87 @@
 //- 데이터 패칭, 이벤트 처리 등의 비즈니스 로직은 컨테이너가 담당한다.
 //- UI 컴포넌트를 컨트롤하는 역할이다.
 
-import {getTodos} from '../apis/TodoList';
-import useAsync from '../hooks/useAsync';
+import {useCallback, useEffect, useRef} from 'react';
+import * as fetcher from '../apis/TodoList';
+import Create from '../components/TodoList/Create';
+import {useTodoDispatch, useTodoState} from '../contexts/TodoList';
 
 const TodoListContainer = () => {
-    const [state] = useAsync(() => getTodos());
-    const {loading, data: todos, error} = state;
+    const todoState = useTodoState();
+    const todoDispatch = useTodoDispatch();
+    const inputRef = useRef<HTMLInputElement | null>(null);
 
-    if (loading) return <div>로딩중..</div>;
-    if (error) return <div>에러가 발생했습니다</div>;
-    if (!todos) return null;
-    if (!todos.data.length) return <div>투두 리스트를 추가해 주세요.</div>;
+    const getTodos = useCallback(async () => {
+        try {
+            const res = await fetcher.getTodos();
+            todoDispatch({type: 'GET', payload: res.data});
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                alert(e.message);
+            } else {
+                console.error('확인할 수 없는 에러 발생');
+            }
+        }
+    }, [todoDispatch]);
+
+    const createTodo = useCallback(async () => {
+        try {
+            const res = await fetcher.createTodo({todo: inputRef.current?.value || ''});
+            todoDispatch({type: 'CREATE', payload: res.data});
+        } catch (e: unknown) {
+            if (e instanceof Error) {
+                alert(e.message);
+            } else {
+                console.error('확인할 수 없는 에러 발생');
+            }
+        }
+    }, [todoDispatch]);
+
+    const updateTodo = useCallback(
+        async (id: number, value: string, isCompleted: boolean) => {
+            try {
+                const res = await fetcher.updateTodo(id, {todo: '', isCompleted: true});
+                todoDispatch({type: 'UPDATE', payload: res.data});
+            } catch (e: unknown) {
+                if (e instanceof Error) {
+                    alert(e.message);
+                } else {
+                    console.error('확인할 수 없는 에러 발생');
+                }
+            }
+        },
+        [todoDispatch]
+    );
+
+    const deleteTodo = useCallback(
+        async (id: number) => {
+            try {
+                const res = await fetcher.deleteTodo(id);
+                todoDispatch({type: 'DELETE', payload: res.data});
+            } catch (e: unknown) {
+                if (e instanceof Error) {
+                    alert(e.message);
+                } else {
+                    console.error('확인할 수 없는 에러 발생');
+                }
+            }
+        },
+        [todoDispatch]
+    );
+
+    useEffect(() => {
+        getTodos();
+    }, [getTodos, todoState]);
+
+    const handleFormSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        createTodo();
+    };
 
     return (
         <>
             <h1>TodoList Container</h1>
+            <Create inputRef={inputRef} handleFormSubmit={handleFormSubmit} />
         </>
     );
 };
