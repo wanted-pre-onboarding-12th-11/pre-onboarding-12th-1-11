e78a18b6b966456d1296487cf7ef62d654f61619
saem <saemilee01@gmail.com>
2023-08-24 02:14:15 +0900

    feat: 컴포넌트 구조 추가

--- src/components/TodoList/Create.tsx
+++ src/components/TodoList/Create.tsx
@@ -1,8 +1,23 @@
-const TodoCreate = () => {
+import {LegacyRef} from 'react';
+
+interface TodoCreateProps {
+    inputRef: LegacyRef<HTMLInputElement> | undefined;
+    handleFormSubmit: (e: React.FormEvent) => void;
+}
+
+const TodoCreate = ({inputRef, handleFormSubmit}: TodoCreateProps) => {
     return (
-        <>
-            <h1>TodoCreate Component</h1>
-        </>
+        <form onSubmit={handleFormSubmit}>
+            <input
+                placeholder='할 일을 입력해 주세요...'
+                data-testid='new-todo-input'
+                type='text'
+                ref={inputRef}
+            />
+            <button data-testid='new-todo-add-button' type='submit'>
+                추가
+            </button>
+        </form>
     );
 };
 
