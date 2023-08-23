a3d28415629fa011da444fe97f508d9f115a19dd
saem <saemilee01@gmail.com>
2023-08-24 02:12:29 +0900

    refactor: 함수 선언식으로 변경

--- src/apis/TodoList.ts
+++ src/apis/TodoList.ts
@@ -3,22 +3,22 @@ import API_KEY from '../constants/apiKey';
 
 const TODO_PATH = '/todos';
 
-export async function getTodos() {
+export const getTodos = async () => {
     const response = await Api.get(API_KEY, TODO_PATH, true);
     return response;
-}
+};
 
-export async function createTodo(todo: {todo: string}) {
+export const createTodo = async (todo: {todo: string}) => {
     const response = await Api.post(API_KEY, TODO_PATH, todo, true);
     return response;
-}
+};
 
-export async function updateTodo(id: number, todo: {todo: string; isCompleted: boolean}) {
+export const updateTodo = async (id: number, todo: {todo: string; isCompleted: boolean}) => {
     const response = await Api.put(API_KEY, `${TODO_PATH}${id}`, todo, true);
     return response;
-}
+};
 
-export async function deleteTodo(id: number) {
+export const deleteTodo = async (id: number) => {
     const response = await Api.delete(API_KEY, `${TODO_PATH}${id}`, true);
     return response;
-}
+};
