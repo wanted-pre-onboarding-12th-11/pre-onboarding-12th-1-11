287d008b7327999f3b00a9ce0200f6cb2722b137
saem <saemilee01@gmail.com>
2023-08-23 23:32:15 +0900

    feat: 컨테이너 연결

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,10 +1,11 @@
 // - 라우팅의 단위가 될 컴포넌트이다.
 // - 단순 래핑의 역할과 SEO를 위한 메타태그 설정의 역할만 한다.
+import TodoListContainer from '../containers/TodoList';
 
 const TodoListPage = () => {
     return (
         <>
-            <h1>TodoList Page</h1>
+            <TodoListContainer />
         </>
     );
 };
