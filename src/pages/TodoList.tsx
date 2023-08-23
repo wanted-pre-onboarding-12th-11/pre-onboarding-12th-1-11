4c7fc010c112f0eabf397cbe798cf13fcf701232
saem <saemilee01@gmail.com>
2023-08-24 02:14:39 +0900

    feat: TodoProvider 추가

--- src/pages/TodoList.tsx
+++ src/pages/TodoList.tsx
@@ -1,11 +1,14 @@
 // - 라우팅의 단위가 될 컴포넌트이다.
 // - 단순 래핑의 역할과 SEO를 위한 메타태그 설정의 역할만 한다.
 import TodoListContainer from '../containers/TodoList';
+import {TodoProvider} from '../contexts/TodoList';
 
 const TodoListPage = () => {
     return (
         <>
-            <TodoListContainer />
+            <TodoProvider>
+                <TodoListContainer />
+            </TodoProvider>
         </>
     );
 };
