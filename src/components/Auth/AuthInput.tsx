e67a3812d9c445c68a732c50f16398ca52fd2839
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 17:10:54 +0900

    feat: Error message component 추가

--- src/components/Auth/AuthInput.tsx
+++ src/components/Auth/AuthInput.tsx
@@ -8,13 +8,18 @@ interface AuthInputProps {
   handleEmail: (e: ChangeEvent<HTMLInputElement>) => void,
   password: string,
   handlePassword: (e: ChangeEvent<HTMLInputElement>) => void,
+  errorMessage: {
+    emailError: string,
+    passwordError: string,
+  }
 }
 
 export const AuthInput = ({
   email,
   handleEmail,
   password,
-  handlePassword
+  handlePassword,
+  errorMessage,
 }: AuthInputProps) => {
 
   console.info('AuthInput re-rendering');
@@ -22,23 +27,25 @@ export const AuthInput = ({
   return (
     <S.AuthInputStyled>
       <Input
+        label='이메일'
         id='eamil'
         type='text'
         testid='email-input'
-        placeholder='user@wanted.com'
+        placeholder='이메일을 입력해주세요.'
         value={email}
         onChange={handleEmail}
       />
-      <AuthErrorMessage message='message' />
+      <AuthErrorMessage message={email === '' ? '' : errorMessage.emailError} />
       <Input
+        label='비밀번호'
         id='password'
         type='password'
         testid='password-input'
-        placeholder='********'
+        placeholder='비밀번호를 입력해주세요.'
         value={password}
         onChange={handlePassword}
       />
-      <AuthErrorMessage message='message' />
+      <AuthErrorMessage message={password === '' ? '' : errorMessage.passwordError} />
     </S.AuthInputStyled>
   )
 }
