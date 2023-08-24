0a4a26e7091648f84e944c8cd9a577a5cbcbd69f
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 17:08:41 +0900

    feat: Auth component import

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
-import React from 'react';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import ROUTES from './constants/routes';
-
+import './styles/global.css'
+import { Signup } from './pages/Signup';
+import { Signin } from './pages/Signin';
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<Navigate to={ROUTES.TODO} />} />
-        <Route path={ROUTES.SIGNIN} />
-        <Route path={ROUTES.SIGNUP} />
+        <Route path={ROUTES.SIGNIN} element={<Signin />} />
+        <Route path={ROUTES.SIGNUP} element={<Signup />} />
         <Route path={ROUTES.TODO} />
         <Route path='/*' element={<Navigate to={ROUTES.TODO} />} />
       </Routes>
