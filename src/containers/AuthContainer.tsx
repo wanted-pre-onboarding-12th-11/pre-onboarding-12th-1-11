41827a568aca64a8e4b35d44d59fab29b45f8420
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 20:06:44 +0900

    feat: 상태 및 핸들러 관리를 useAuth로 분리

--- src/containers/AuthContainer.tsx
+++ src/containers/AuthContainer.tsx
@@ -6,12 +6,10 @@ import { AuthFooter } from '../components/Auth/AuthFooter';
 import * as S from '../styles/Auth.styled';
 
 // custom hook, api, validation import 
-import { useInput } from '../hooks/useInput';
-import { emailValidation, passwordValidation } from '../utils/validation';
+import useAuth from '../hooks/useAuth';
 
 // library import 
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
 
 // constant data import 
 import { EMAIL_VALIDATION_MSG, PASSWORD_VALIDATION_MSG } from '../constants/message';
@@ -19,55 +17,45 @@ import { EMAIL_VALIDATION_MSG, PASSWORD_VALIDATION_MSG } from '../constants/mess
 // types import
 import { AuthPageProps } from '../types/AuthTypes';
 
+
 export const AuthContainer = ({ api, title, navigation, link, buttonType, footerType, testid, footerText }: AuthPageProps) => {
-  const navigate = useNavigate();
+  const { form, onChange, onSubmit, isSignValid } = useAuth({ api, navigation });
 
-  const [email, handleEmail] = useInput('');
-  const [password, handlePassword] = useInput('');
 
-  // 에러 메시지 관리 방안 고민중 
   const [errorMessage, setErrorMessage] = useState({
     emailError: '',
     passwordError: '',
   })
 
-  const isEmailValid = emailValidation(email)
-  const isPasswordValid = passwordValidation(password)
-
   useEffect(() => {
     const updatedErrorMessage = {
       ...errorMessage,
-      emailError: isEmailValid ? '' : EMAIL_VALIDATION_MSG,
-      passwordError: isPasswordValid ? '' : PASSWORD_VALIDATION_MSG
+      emailError: form.email.isValid ? '' : EMAIL_VALIDATION_MSG,
+      passwordError: form.password.isValid ? '' : PASSWORD_VALIDATION_MSG,
     };
 
     setErrorMessage(updatedErrorMessage);
 
-  }, [isEmailValid, isPasswordValid]);
+  }, [form.email.isValid, form.password.isValid]);
 
-
-  // 렌더링 최적화 실패
-  const handleSubmit = async () => {
-    await api(email, password);
-    navigate(navigation);
-  }
 
   return (
     <S.AuthContainerStyled>
       <AuthTitle title={title} />
-      <form>
+      <form >
         <AuthInput
-          email={email}
-          handleEmail={handleEmail}
-          password={password}
-          handlePassword={handlePassword}
+          email={form.email.value}
+          handleEmail={onChange}
+          password={form.password.value}
+          handlePassword={onChange}
           errorMessage={errorMessage}
         />
         <Button
           type={buttonType}
           testid={testid}
-          disabled={!emailValidation(email) || !passwordValidation(password)}
-          onClick={handleSubmit} />
+          disabled={!isSignValid}
+          onClick={onSubmit}
+        />
       </form>
       <AuthFooter
         text={footerText}
