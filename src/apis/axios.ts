f94679c5e7828bb66f4ff21afe194fbc021447a7
Hyesu <honghs95@gmail.com>
2023-08-24 22:36:34 +0900

    remove: apis 폴더에서 사용하지 않는 파일 삭제

--- src/apis/axios.ts
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import API_KEY from '../constants/apiKey';
-
-export const signAxios = axios.create({
-    baseURL: API_KEY,
-    headers: {
-        'Content-Type': 'application/json',
-    },
-});
