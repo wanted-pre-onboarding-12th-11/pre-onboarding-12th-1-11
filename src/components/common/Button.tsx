6a2a871b9e962e2d6dc224cbdc5764d4935dcb72
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 17:11:38 +0900

    feat: Button style 적용

--- src/components/common/Button.tsx
+++ src/components/common/Button.tsx
@@ -1,17 +1,21 @@
+// react import 
 import { memo } from 'react';
+// type, style import 
 import { ButtonProps } from '../../types/AuthTypes';
+import * as S from '../../styles/Common.styled';
+
 
 export const Button = memo(({ type, testid, disabled, onClick }: ButtonProps) => {
   console.info('Button re-rendering');
 
   return (
-    <button
+    <S.ButtonStyled
       type='button'
       data-testid={testid}
       disabled={disabled}
       onClick={onClick}
     >
       {type}
-    </button>
+    </S.ButtonStyled>
   )
 })
