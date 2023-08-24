8de14d391cea2fe04fe7572e4e9880db6400ed37
saem <saemilee01@gmail.com>
2023-08-24 20:00:39 +0900

    feat: 메타태그 추가

--- src/pages/Todo.tsx
+++ src/pages/Todo.tsx
@@ -1,15 +1,17 @@
 // - 라우팅의 단위가 될 컴포넌트이다.
 // - 단순 래핑의 역할과 SEO를 위한 메타태그 설정의 역할만 한다.
+import MetaTag from '../components/SEO/MetaTag';
 import TodoContainer from '../containers/Todo';
 import {TodoProvider} from '../contexts/Todo';
 
 const TodoPage = () => {
     return (
-        <>
+        <div>
+            <MetaTag title='투두리스트' description='11팀 투두리스트' />
             <TodoProvider>
                 <TodoContainer />
             </TodoProvider>
-        </>
+        </div>
     );
 };
 
