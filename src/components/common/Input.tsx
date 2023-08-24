599004df48a5270284037969a22fd17df401a6de
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 12:08:00 +0900

    feat: Input component 생성

---
+++ src/components/common/Input.tsx
@@ -0,0 +1,26 @@
+import { ChangeEvent } from 'react';
+
+interface InputProps {
+  id: string;
+  type: string;
+  testid: string;
+  placeholder?: string;
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export const Input = ({ testid, id, type, placeholder, value, onChange }: InputProps) => {
+  return (
+    <div className='input'>
+      <label htmlFor={id} />
+      <input
+        id={id}
+        type={type}
+        data-testid={testid}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  )
+}
