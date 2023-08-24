5abced1348045dbf9b1a57581ba21f777d467fc4
Aroma-oh <115550622+Aroma-oh@users.noreply.github.com>
2023-08-24 13:37:27 +0900

    feat: AuthErrorMessage component 생성

---
+++ src/components/Auth/AuthErrorMessage.tsx
@@ -0,0 +1,12 @@
+import { memo } from 'react'
+import * as S from '../../styles/Auth.styled'
+
+export const AuthErrorMessage = memo(({ message }: { message: string }) => {
+  console.info('AuthErrorMessage re-rendering');
+
+  return (
+    <S.AuthErrorMessageStyled>
+      {message}
+    </S.AuthErrorMessageStyled>
+  )
+})
