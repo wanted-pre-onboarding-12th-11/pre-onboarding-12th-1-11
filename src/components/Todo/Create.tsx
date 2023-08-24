dd36f7d643be73acca0ea0becc9d247d32f05250
saem <saemilee01@gmail.com>
2023-08-24 23:00:24 +0900

    feat: 투두 글자수 제한

--- src/components/Todo/Create.tsx
+++ src/components/Todo/Create.tsx
@@ -1,23 +1,31 @@
 import {useRef} from 'react';
 import Button from '../common/Button';
 import * as S from '../../styles/Todo.styled';
+import {useInput} from '../../hooks/useInput';
 
 interface TodoCreateProps {
     createTodo: (value: string) => void;
 }
 
 const TodoCreate = ({createTodo}: TodoCreateProps) => {
+    const [value, handleInputChange, setValue] = useInput('');
     const inputRef = useRef<HTMLInputElement | null>(null);
 
     const handleFormSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        const value = inputRef.current?.value || null;
-        if (value && value !== '') {
-            createTodo(value || '');
+        if (value.length) {
+            createTodo(value);
         }
-        if (inputRef.current) {
-            inputRef.current.value = '';
-            inputRef.current.focus();
+        setValue('');
+        inputRef.current?.focus();
+    };
+
+    const handleCheckLength = (e: React.ChangeEvent<HTMLInputElement>) => {
+        handleInputChange(e);
+        if (value.length > 29) {
+            setValue(() => {
+                return value.substring(0, 30);
+            });
         }
     };
 
@@ -27,6 +35,8 @@ const TodoCreate = ({createTodo}: TodoCreateProps) => {
                 placeholder='할 일을 입력해 주세요...'
                 data-testid='new-todo-input'
                 type='text'
+                value={value}
+                onChange={handleCheckLength}
                 ref={inputRef}
             />
 
